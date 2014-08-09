b52c014da72ac9cef18370cfd69132ece87991aa
Liang.Michael <xzc2116@gmail.com>
2014-08-09 09:37:56 +0800

    Initial Commit

---
+++ Countdown/XZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XZAppDelegate.h
+//  Countdown
+//
+//  Created by Liang on 8/9/14.
+//  Copyright (c) 2014 Xing Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Countdown/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Countdown
+//
+//  Created by Liang on 8/9/14.
+//  Copyright (c) 2014 Xing Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XZAppDelegate class]));
+    }
+}
